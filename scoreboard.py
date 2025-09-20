cfe23fc2b1befe7a24b3c3314250614c8575a669
Kschmitt11 <kenley.schmitt11@gmail.com>
2025-09-20 10:10:11 -0500

    Saves high scores to file

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("high_score.txt") as data:
+            self.high_score = int(data.read())
         self.options()
 
     def options(self):
@@ -26,6 +27,8 @@ class Scoreboard(Turtle):
     def reset_scoreboard(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("high_score.txt", "w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.clear()
         self.options()
