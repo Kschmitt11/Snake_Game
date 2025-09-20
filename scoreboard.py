d51152c6934f9ec9afe2c78de396fcaaa27a987e
Kschmitt11 <kenley.schmitt11@gmail.com>
2025-09-20 09:09:24 -0500

    Added high score functionality

--- scoreboard.py
+++ scoreboard.py
@@ -8,19 +8,29 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.options()
 
     def options(self):
         self.hideturtle()
         self.goto(0, 260)
         self.pencolor("white")
-        self.write(f"Score = {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score = {self.score} High Score = {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def update(self):
         self.clear()
         self.score += 1
         self.options()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def reset_scoreboard(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.clear()
+        self.options()
+
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
--- snake.py
+++ snake.py
@@ -38,6 +38,13 @@ class Snake:
             self.segments[seg_num].goto(new_x,new_y)
         self.head.fd(MOVE_DISTANCE)
 
+    def reset(self):
+        for segment in self.segments:
+            segment.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
